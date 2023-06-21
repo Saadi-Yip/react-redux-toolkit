dc5f3eb1b4e8c2734eae9f961772b0e07799ad03
Saadi-Yip <saadshabbir@leilanitech.com>
2023-06-21 15:01:45 +0500

    redux persistor added

--- src/components/store/index.jsx
+++ src/components/store/index.jsx
@@ -9,3 +9,48 @@ const store = configureStore({
     }
 });
 export default store;
+
+// Persisitor configuration
+//npm install redux-persist
+// import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// // Import your reducers
+// import rootReducer from './reducers';
+
+// // Configuration for Redux Persist
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   // Add any additional configuration options if needed
+// };
+
+// // Create the persisted reducer
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// // Create the store
+// export const store = configureStore({
+//   reducer: persistedReducer,
+// });
+
+// // Create the persistor
+// export const persistor = persistStore(store);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+// import { store, persistor } from './store';
+// import App from './App';
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux'
-import store from './components/store';
+import store, { persistor } from './components/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
